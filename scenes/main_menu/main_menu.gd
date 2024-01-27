e84e399b1fbcd34b6c84a01b82ead9fb46639bb8
Kauê Cavalcante <kaue_cavalcante.cnt@outlook.com>
2024-01-27 19:04:38 -0300

    Implemented rules scene

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -2,3 +2,7 @@ extends Control
 
 func _on_play_button_button_down():
 	get_tree().change_scene_to_file("res://scenes/create_players/create_players.tscn")
+
+
+func _on_rules_button_button_down():
+	get_tree().change_scene_to_file("res://scenes/rules/rules.tscn")
