0ca8d00bf8f130e1b4b55f7fd308f08c7a229a89
Kauê Cavalcante <kaue_cavalcante.cnt@outlook.com>
2024-01-27 11:32:47 -0300

    Added label to indicate judge player

--- commons/player_name_label/player_name_label.gd
+++ commons/player_name_label/player_name_label.gd
@@ -1,6 +1,11 @@
 extends Label
 
 @onready var game_controller = GameController
+@onready var session_controller = SessionController
 
 func _ready():
-	text = "Turn: " + game_controller.current_player.name
+	var current_player = game_controller.current_player
+	var judge = session_controller.current_round.judge
+	text = "Turn: " + current_player.name
+	if current_player.id == judge.id:
+		text += "\n Judge"
